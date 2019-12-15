01161362e7c7453ddb6ce6141198a4b973259da7
Francisco  Inoque <40237204+frantchessico@users.noreply.github.com>
2019-12-15 13:39:39 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,15 @@
+
 export const environment = {
   production: true,
   firebase: {
-  apiKey: "AIzaSyARa4tFRgxqgsDr7_RE_oJ4FZKrcx8Ni4Y",
-  authDomain: "franciscobrainstorm.firebaseapp.com",
-  databaseURL: "https://franciscobrainstorm.firebaseio.com",
-  projectId: "franciscobrainstorm",
-  storageBucket: "franciscobrainstorm.appspot.com",
-  messagingSenderId: "1042346804564",
-  appId: "1:1042346804564:web:554ed4b7170f7361ffb4c1",
-  measurementId: "G-6JSXSJC8FP"
+    apiKey: '#',
+    authDomain: '#',
+    databaseURL: '#',
+    projectId: '#',
+    storageBucket: '#',
+    messagingSenderId: '#',
+    appId: '#',
+    measurementId: '#'
   }
 };
+
